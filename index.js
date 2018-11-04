0f3f149ac345ead70612bb88d933814aedb4e7d1
Toni Abishek James <tjamesch@uwo.ca>
2018-11-04 20:33:11 +0000

    Got routing working from static folder

--- index.js
+++ index.js
@@ -12,14 +12,38 @@ console.log("Server at IP: "+process.env.IP+" port: "+process.env.Port);
 */
 
 //Listing 1b-same functionality as 1a but organized differently
-function serveRequest(request, response)
+/*function serveRequest(request, response)
 {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.end("Hello World\n");
+	//response.end("Hello World\n");
+	response.write(request.method);//write first, cause end is for the last write
+	response.end(request.url);//Step 14, modifying code to display method and url of the request on the browser webpage.
 }
 
 var http = require('http');
 
 var server = http.createServer(serveRequest);
 server.listen(process.env.PORT, process.env.IP);
-console.log("Server at IP:" + process.env.IP + " port: "+process.env.PORT);
+console.log("Server at IP:" + process.env.IP + " port: "+process.env.PORT);*/
+
+var express = require('express');
+var app = express();  
+
+// serve files in static' folder at root URL '/'
+app.use('/', express.static('static'));
+
+app.route('/api')
+ .get(function(req, res) {
+   res.send('Got a GET request for /api');
+ })
+ .post(function(req, res) {
+   res.send('Got a POST request for /api');
+ })
+ .put(function(req, res) {
+   res.send('Got a PUT request for /api');
+ });
+
+app.listen(8080); // start server
+
+console.log('Listening on port 8080');
+
